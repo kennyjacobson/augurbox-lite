1c42021bb7edc5cbea9a50d80cb98d07fc498020
kennyjacobson <kenny.jacobson@gmail.com>
2022-10-13 22:52:27 -0700

    birthday

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
-import {Button} from '@mui/material'
+import {Button, Typography} from '@mui/material'
+
+const answers = [
+  "Most Definitely Yes",
+  "Cloudy, Try Again Later",
+  "No Way",
+  "Affirmative",
+  "Negative",
+  "OK",
+  "Why Not?",
+  "Not A Chance"
+]
 
 function App() {
+  const [message, setMessage] = useState("Hi")
+  const [buttonText, setButtonText] = useState("Your Answer")
+
+  const getRandomPhrase = () => {
+    const i = Math.floor(Math.random() * answers.length)
+    console.log(i)
+    setMessage(answers[i])
+    setButtonText("Your Answer")
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-          Learn React
-        </a>
-        <Button variant="outlined" >Learn React</Button>
+        <Typography sx={{mb:2}} variant='h3'>Ask A Question</Typography>
+        <Typography sx={{mb:4}} variant='h1'>{message}</Typography>
+        <Button variant="contained" onClick={getRandomPhrase} >{buttonText}</Button>
       </header>
     </div>
   );
